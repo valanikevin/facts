1cb0f8d1c96efa706bcdda33db77f171e0046b55
Kevin Valani <valanikevin@gmail.com>
2024-02-14 18:59:03 -0700

    added retrieval qa chain

---
+++ prompt.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from langchain.vectorstores.chroma import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+chat = ChatOpenAI()
+
+db = Chroma(
+    persist_directory="emb",
+    embedding_function=embeddings,
+)
+
+retriever = db.as_retriever()
+
+
+chain = RetrievalQA.from_chain_type(
+    llm=chat,
+    retriever=retriever,
+    chain_type="stuff",
+)
+
+result = chain.run("What is an interesting fact about the English language?")
+
+print(result)
